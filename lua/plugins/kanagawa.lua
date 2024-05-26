f94c4754e328e8d73e1d237347ef64202ea4b2e7
dartey25 <maxxcake@outlook.com>
2024-05-27 01:39:54 +0300

    upd

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -12,11 +12,24 @@ return {
       transparent_mode = true,
     },
   },
+  {
+    "sainnhe/everforest",
+    name = "everforest",
+    config = function()
+      vim.g.everforest_transparent_background = 2
+      vim.g.everforest_ui_contrast = "high"
+      vim.g.everforest_inlay_hints_background = "none"
+    end,
+  },
+  {
+    "TheNiteCoder/mountaineer.vim",
+    name = "mountaineer",
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "everforest",
     },
   },
 }
